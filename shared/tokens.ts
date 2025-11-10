a0d049f88e97422c6c25f3511fe03e559b8eae77
Kolosov Nikita <N.Kolosov2003@mail.ru>
2025-11-10 20:39:17 +0300

    feat(ui): added constants for UI tokens

---
+++ shared/tokens.ts
@@ -0,0 +1,21 @@
+export const COLORS = {
+  black: "#16171D",
+  violetDark: "#2E2D3D",
+  white: "#FFFFFF",
+
+  primary: "#6C38CC",
+  primaryHover: "#452481",
+  link: "#A97BFF",
+
+  gray: "#AFB2BF",
+} as const
+
+export const GAPS = {
+  g16: 16,
+  g26: 26,
+  g50: 50,
+} as const
+
+export const RADIUSES = {
+  r10: 10,
+} as const
