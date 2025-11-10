f0da669722eca656131edfa5ac02b7ae6faca140
Kolosov Nikita <N.Kolosov2003@mail.ru>
2025-11-10 20:39:59 +0300

    feat(ui): added custom Input

---
+++ shared/Input/Input.tsx
@@ -0,0 +1,80 @@
+import { useState } from "react"
+import {
+  TextInput,
+  TextInputProps,
+  StyleSheet,
+  Pressable,
+  View,
+  Text,
+} from "react-native"
+
+import { RADIUSES, COLORS } from "../tokens"
+
+import { EyeClosedIcon } from "../Icons/EyeClosed"
+import { EyeOpenedIcon } from "../Icons/EyeOpened"
+
+interface IInputProps extends TextInputProps {
+  type?: "password"
+}
+
+export const Input = ({
+  style,
+  placeholderTextColor,
+  type,
+  ...props
+}: IInputProps) => {
+  const [isPasswordVisible, setIsPasswordVisible] = useState(false)
+
+  return (
+    <View style={styles.container}>
+      <TextInput
+        style={{
+          ...styles.input,
+          ...Object.values(style ?? {}),
+        }}
+        placeholderTextColor={placeholderTextColor || COLORS.gray}
+        secureTextEntry={type === "password" && !isPasswordVisible}
+        {...props}
+      />
+
+      {type === "password" && (
+        <Pressable
+          style={styles.eyeIcon}
+          onPress={() => setIsPasswordVisible((prev) => !prev)}
+        >
+          {isPasswordVisible ? <EyeOpenedIcon /> : <EyeClosedIcon />}
+        </Pressable>
+      )}
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    position: "relative",
+  },
+  input: {
+    height: 58,
+
+    borderRadius: RADIUSES.r10,
+    paddingBlock: 10,
+    paddingHorizontal: 26,
+
+    backgroundColor: COLORS.violetDark,
+
+    fontFamily: "FiraSans, FiraSans-Regular",
+    fontWeight: 400,
+    fontSize: 18,
+    lineHeight: 1.2,
+    color: COLORS.gray,
+  },
+  eyeIcon: {
+    position: "absolute",
+    right: 0,
+    top: 0,
+    bottom: 0,
+    paddingRight: 24,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+})
