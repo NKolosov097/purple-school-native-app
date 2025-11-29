54f15582bab43431e04f2572e1d523206b80c7e3
Kolosov Nikita <N.Kolosov2003@mail.ru>
2025-11-29 11:04:06 +0300

    feat(notification): added hook for using notification api

---
+++ features/model/useNotification.tsx
@@ -0,0 +1,17 @@
+import { useContext } from "react"
+
+import { ToastContext } from "@app/providers/notification-provider/notification.provider"
+
+export const useNotification = () => {
+  const context = useContext(ToastContext)
+  if (!context) {
+    throw new Error("useToast must be used within a ToastProvider")
+  }
+
+  return {
+    default: (title: string) => context.showToast("default", title),
+    info: (title: string) => context.showToast("info", title),
+    success: (title: string) => context.showToast("success", title),
+    error: (title: string) => context.showToast("error", title),
+  }
+}
