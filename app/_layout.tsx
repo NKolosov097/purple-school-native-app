c62aba7689d852508a5304f4f412537bf0cfb546
Kolosov Nikita <N.Kolosov2003@mail.ru>
2025-12-07 10:26:08 +0300

    fix(splash): updated splash screen

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,15 +1,18 @@
-import { useMemo } from "react"
+import { useEffect, useMemo } from "react"
 import { StatusBar } from "react-native"
 import { useSafeAreaInsets } from "react-native-safe-area-context"
 
+import Constants from "expo-constants"
 import { useFonts } from "expo-font"
-import { Stack } from "expo-router"
+import { SplashScreen, Stack } from "expo-router"
 import "react-native-reanimated"
 
 import { Providers } from "@/providers"
 
 import { StackProps } from "@/shared/types/global.types"
 import { COLORS } from "@shared/config/tokens"
+
+SplashScreen.preventAutoHideAsync()
 
 export default function RootLayout() {
   const {
@@ -33,14 +36,22 @@ export default function RootLayout() {
     [paddingTop, paddingBottom, paddingLeft, paddingRight]
   )
 
-  const [fontsLoaded] = useFonts({
+  const [fontsLoaded, fontsError] = useFonts({
     FiraSans: require("../assets/fonts/FiraSans-Regular.ttf"),
     "FiraSans-SemiBold": require("../assets/fonts/FiraSans-SemiBold.ttf"),
   })
 
-  if (!fontsLoaded) {
-    return null
-  }
+  useEffect(() => {
+    if (fontsLoaded) {
+      SplashScreen.hideAsync()
+    }
+  }, [fontsLoaded])
+
+  useEffect(() => {
+    if (fontsError && Constants.debugMode) {
+      throw fontsError
+    }
+  }, [fontsError])
 
   return (
     <Providers>
