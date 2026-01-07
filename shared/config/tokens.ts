81f8f37033b9799c211c0a55abb77f3ad522d8c2
Kolosov Nikita <N.Kolosov2003@mail.ru>
2026-01-07 17:04:04 +0300

    feat(tokens): added new ui tokens

--- shared/config/tokens.ts
+++ shared/config/tokens.ts
@@ -7,8 +7,12 @@ export const COLORS = {
   primary: "#6C38CC",
   primaryHover: "#452481",
   link: "#A97BFF",
+  pink: "#E47AD5",
+  pinkLight: "#D77BE5",
 
   gray: "#AFB2BF",
+  grayStroke: "#4D5064",
+  grayLight: "#DAD9DE",
 
   success: "#10B981",
   error: "#CC384E",
@@ -18,7 +22,9 @@ export const COLORS = {
 
 export const GAPS = {
   g8: 8,
+  g12: 12,
   g16: 16,
+  g18: 18,
   g24: 24,
   g32: 32,
   g50: 50,
@@ -26,15 +32,19 @@ export const GAPS = {
 
 export const RADIUSES = {
   r10: 10,
+  r17: 17,
+  r20: 20,
 } as const
 
 export const FONTS = {
   "FiraSans-Regular": "FiraSans-Regular",
   "FiraSans-SemiBold": "FiraSans-SemiBold",
+  f12: 12,
   f14: 14,
   f16: 16,
   f18: 18,
   f20: 20,
+  f24: 24,
 } as const
 
 export const AVATAR_SIZE = 70 as const
